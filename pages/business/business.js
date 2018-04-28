43102bdee2a360f7fdf5c5e546c95138357dcf74
darcrand <darcrandex@gmail.com>
2018-04-28 18:20:56 +0800

    完成民宿列表,开始做详情

--- pages/business/business.js
+++ pages/business/business.js
@@ -1,1 +1,48 @@
-Page({})
+import { observer, mapStore } from '../../store/tools'
+
+Page({
+  data: {
+    list: [
+      {
+        id: '',
+        img:
+          'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1524914050487&di=d7025c859bf5f1f3264d010aae46c348&imgtype=0&src=http%3A%2F%2Ffile28.mafengwo.net%2FM00%2F0A%2F69%2FwKgB6lPIvI6AE9diAAU1YYsk2Ro53.rbook_comment.w1024.jpeg',
+        level: 2,
+        name: '小酒馆sdjofjdjfoiewjfojewofijewiofjeiwojfiewjrioioewriowpekfopwekropweirp',
+        score: 4.5,
+        tags: ['免押金', '高配置', '豪华'],
+        price: '99999'
+      },
+      {
+        id: '',
+        img:
+          'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1524914050487&di=d7025c859bf5f1f3264d010aae46c348&imgtype=0&src=http%3A%2F%2Ffile28.mafengwo.net%2FM00%2F0A%2F69%2FwKgB6lPIvI6AE9diAAU1YYsk2Ro53.rbook_comment.w1024.jpeg',
+        level: 2,
+        name: '小酒馆sdjofjdjfoiewjfojewofijewiofjeiwojfiewjrioioewriowpekfopwekropweirp',
+        score: 4.5,
+        tags: ['免押金', '高配置', '豪华'],
+        price: '999'
+      },
+      {
+        id: '',
+        img:
+          'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1524914050487&di=d7025c859bf5f1f3264d010aae46c348&imgtype=0&src=http%3A%2F%2Ffile28.mafengwo.net%2FM00%2F0A%2F69%2FwKgB6lPIvI6AE9diAAU1YYsk2Ro53.rbook_comment.w1024.jpeg',
+        level: 2,
+        name: '小酒馆sdjofjdjfoiewjfojewofijewiofjeiwojfiewjrioioewriowpekfopwekropweirp',
+        score: 4.5,
+        tags: ['免押金', '高配置', '豪华'],
+        price: '999'
+      },
+      {
+        id: '',
+        img:
+          'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1524914050487&di=d7025c859bf5f1f3264d010aae46c348&imgtype=0&src=http%3A%2F%2Ffile28.mafengwo.net%2FM00%2F0A%2F69%2FwKgB6lPIvI6AE9diAAU1YYsk2Ro53.rbook_comment.w1024.jpeg',
+        level: 2,
+        name: '小酒馆sdjofjdjfoiewjfojewofijewiofjeiwojfiewjrioioewriowpekfopwekropweirp',
+        score: 4.5,
+        tags: ['免押金', '高配置', '豪华'],
+        price: '999'
+      }
+    ]
+  }
+})
